7e7e285e26df6dada065bab497e9421a55fdfdd4
Marc Chesebro <marc.chesebro@gmail.com>
2017-11-04 13:57:15 -0400

    Added the remaining monsters and added a helper function to generate a random monster

--- Entities/Monsters.py
+++ Entities/Monsters.py
@@ -17,3 +17,39 @@ class Zombie(Entity):
         super(Zombie, self).__init__(health=randint(50, 100), attack=1)
     def dealDamage(self, target):
         super(Zombie, self).dealDamage(target, randint(0, 10))
+
+class Vampire(Entity):
+    """A Vampire Monster that is not harmed by Chocolate bars or SourStraws"""
+    def __init__(self):
+        super(Vampire, self).__init__(health=randint(100, 200), attack=1)
+    def dealDamage(self, target):
+        super(Vampire, self).dealDamage(target, randint(10, 20))
+
+class Ghoul(Entity):
+    """A Ghoul Monster that takes 5X the damage from NerdBombs"""
+    def __init__(self):
+        super(Ghoul, self).__init__(health=randint(40, 80), attack=1)
+    def dealDamage(self, target):
+        super(Ghoul, self).dealDamage(target, randint(15, 30))
+
+class Werewolf(Entity):
+    """A Ghoul Monster that takes 5X the damage from NerdBombs"""
+    def __init__(self):
+        super(Werewolf, self).__init__(health=200, attack=1)
+    def dealDamage(self, target):
+        super(Werewolf, self).dealDamage(target, randint(0, 40))
+
+def generateRandomMonster():
+    monsterInt = randint(0, 4)
+    if(monsterInt == 0):
+        return Person()
+    elif(monsterInt == 1):
+        return Zombie()
+    elif(monsterInt == 2):
+        return Vampire()
+    elif(monsterInt == 3):
+        return Ghoul()
+    elif(monsterInt == 4):
+        return Werewolf()
+    else:
+        return None
