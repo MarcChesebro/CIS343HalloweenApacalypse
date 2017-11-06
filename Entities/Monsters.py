9838e46dafb450dd673fe4751255570dedcc24e8
Marc Chesebro <marc.chesebro@gmail.com>
2017-11-06 10:44:45 -0500

    implemented __str__ methods and fixed takeDamge method for all monsters

--- Entities/Monsters.py
+++ Entities/Monsters.py
@@ -8,13 +8,20 @@ class Person(Entity):
     def __init__(self):
         super(Person, self).__init__(100, -1)
 
+    def __str__(self):
+        return "Human"
+
     # overrides the take damage constructor to take 0 damage from attacks
-    def takeDamage(amount):
+    def takeDamage(self, amount, weapon):
         super(Person, self).takeDamage(0)
 
 class Zombie(Entity):
     def __init__(self):
         super(Zombie, self).__init__(health=randint(50, 100), attack=1)
+
+    def __str__(self):
+        return "Zombie"
+
     def dealDamage(self, target):
         super(Zombie, self).dealDamage(target, randint(0, 10))
 
@@ -22,6 +29,10 @@ class Vampire(Entity):
     """A Vampire Monster that is not harmed by Chocolate bars or SourStraws"""
     def __init__(self):
         super(Vampire, self).__init__(health=randint(100, 200), attack=1)
+
+    def __str__(self):
+        return "Vampire"
+
     def dealDamage(self, target):
         super(Vampire, self).dealDamage(target, randint(10, 20))
 
@@ -29,6 +40,10 @@ class Ghoul(Entity):
     """A Ghoul Monster that takes 5X the damage from NerdBombs"""
     def __init__(self):
         super(Ghoul, self).__init__(health=randint(40, 80), attack=1)
+
+    def __str__(self):
+        return "Ghoul"
+
     def dealDamage(self, target):
         super(Ghoul, self).dealDamage(target, randint(15, 30))
 
@@ -36,6 +51,10 @@ class Werewolf(Entity):
     """A Ghoul Monster that takes 5X the damage from NerdBombs"""
     def __init__(self):
         super(Werewolf, self).__init__(health=200, attack=1)
+
+    def __str__(self):
+        return "Werewolf"
+
     def dealDamage(self, target):
         super(Werewolf, self).dealDamage(target, randint(0, 40))
 
--- Entities/Player.py
+++ Entities/Player.py
@@ -5,6 +5,7 @@ class Player(Entity):
 
     def __init__(self):
         self._weapons = Weapons.generateRandomWeaponList(10)
+        super(Player, self).__init__()
 
     def dealDamage(self, house, weaponNum):
-        house.takeDamage(weapons[weaponNum].use(self.attack), weapons[weaponNum])
+        house.takeDamage(self._weapons[weaponNum].use(self._attack), self._weapons[weaponNum])
