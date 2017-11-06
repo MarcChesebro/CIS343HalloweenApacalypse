03f0dc0fb3f07029e6026634c1ab44b3828ae240
Marc Chesebro <marc.chesebro@gmail.com>
2017-11-06 10:53:13 -0500

    implemented Nieghborhood

--- Nieghborhood.py
+++ Nieghborhood.py
@@ -1,3 +1,11 @@
+from House import House
 
 class Nieghborhood(object):
-    houses[][];
+    def __init__(self, hieght=5, width=5):
+        self._houses = [[House() for x in range(width)] for y in range(hieght)]
+
+    def attack(self, source, x, y):
+        # tell the player which house to attack and deal damage to all the monsters
+        source.dealDamage(_houses[x][y])
+        # loop through monsters in the house and see if any died(health < 0)
+        # if it is tell the house to replace it with a Person
