0640cf579bb3491d7d2a21b2c813e84b21fc9d5c
Marc Chesebro <marc.chesebro@gmail.com>
2017-11-06 10:43:52 -0500

    changed vafiables to be private and added output when they take damage

--- Entities/Entity.py
+++ Entities/Entity.py
@@ -1,11 +1,13 @@
 
 class Entity(object):
     def __init__(self, health = 100, attack = 10):
-        self.health = health
-        self.attack = attack
+        self._health = health
+        self._attack = attack
 
-    def takeDamage(self, amount):
-        self.health = self.health - amount
+    def takeDamage(self, amount, weapon=None):
+        self._health = self._health - amount
+        print("{} takes {} damage! \t current health: {}".format(self, amount, self._health))
+
 
     def dealDamage(self, target, modifier=1):
-        target.takeDamage(self.attack * modifier)
+        target.takeDamage(self._attack * modifier)
