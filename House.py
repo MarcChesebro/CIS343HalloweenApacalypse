c325921730d0036ab71ca7ce1ef4be27fd5b4a92
Marc Chesebro <marc.chesebro@gmail.com>
2017-11-07 14:00:16 -0500

    made gererateMonsters now add self as an observer when the monsters are created

--- House.py
+++ House.py
@@ -4,23 +4,23 @@ from Observers.Observer import Observer
 
 class House(Observer):
     def __init__(self):
-        self._monsters = House.generateMonsters()
+        self._monsters = self.generateMonsters()
 
-    def update(self):
-        print("house was updated")
+    def update(self, info):
+        if info in self._monsters:
+            self._monsters[self._monsters.index(info)] = Monsters.Person()
 
     def takeDamage(self, amount, weapon):
         for i, mon in enumerate(self._monsters):
             mon.takeDamage(amount, weapon)
-            if mon.isDead:
-                self._monsters[i] = Monsters.Person()
 
     def dealDamage(self, player):
         for Mon in _monsters:
             mon.dealDamage(player)
 
-    def generateMonsters():
+    def generateMonsters(self):
         monsterList = []
         for i in range(0, randint(0, 10)):
             monsterList.append(Monsters.generateRandomMonster())
+            monsterList[len(monsterList) - 1].add_observer(self)
         return monsterList
