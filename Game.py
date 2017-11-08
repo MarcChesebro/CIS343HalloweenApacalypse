56f458c006f00316c0299984b83e5b5ce3d7d1ec
Marc Chesebro <marc.chesebro@gmail.com>
2017-11-08 10:11:52 -0500

    implemented move function used to move the player around

--- Game.py
+++ Game.py
@@ -7,3 +7,9 @@ class Game(object):
         self.player = Player()
         self.playerLoc = (0, 0)
         self.neighborhood = Neighborhood()
+
+        def move(self, dir):
+            if dir[0] in range(0, 1) && dir[1] in range(0, 1):
+                newLoc =  (self.playerLoc[0] + dir[0], self.playerLoc[1] + dir[1])
+                if self.neighborhood.inBounds(newLoc)
+                    self.playerLoc = newLoc
