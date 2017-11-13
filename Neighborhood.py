5f6cacffebcb65f1146d7b46de418b89853d8708
Marc Chesebro <marc.chesebro@gmail.com>
2017-11-13 09:46:15 -0500

    made player-neighborhood interaction more OO

--- Neighborhood.py
+++ Neighborhood.py
@@ -4,11 +4,11 @@ class Neighborhood(object):
     def __init__(self, height=5, width=5):
         self._houses = [[House() for x in range(width)] for y in range(height)]
 
-    def attack(self, source, weapon, x, y):
-        # tell the player which house to attack and deal damage to all the monsters
-        self._houses[x][y].attackAll(source, weapon)
-        # have the monster deal damage back
-        self._houses[x][y].dealDamage(source)
+    def attack(self, source, weapon, loc):
+
+        source.attackAll(self._houses[loc[0]][loc[1]], weapon)
+        # have the monsters deal damage back
+        self._houses[loc[0]][loc[1]].dealDamage(source)
 
     def getHouse(self, loc):
         return self._houses[loc[0]][loc[1]]
