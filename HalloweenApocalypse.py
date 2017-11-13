e6f3f257c18252f4f246f870d649d011f200ecb6
Marc Chesebro <marc.chesebro@gmail.com>
2017-11-13 10:45:53 -0500

    implemented simple main method to move around attack and peek into houses

--- HalloweenApocalypse.py
+++ HalloweenApocalypse.py
@@ -1,12 +1,30 @@
+#!/usr/bin/env python3
+
 from Game import Game
 
 game = Game()
+commands = ["m", "a", "p"]
 
-while True:
-    print("what house would you like to attack: \n")
-    hx = int(input("x: \n"))
-    hy = int(input("y: \n"))
-    print("choose weapon: \n")
-    game.player.printWeapons()
-    wNum = int(input("Choice: \n"))
-    game.neighborhood.attack(game.player, hx, hy)
+if __name__ == "__main__":
+
+    while True:
+        print("location: {}".format(game.getPlayerLoc()))
+        command = input("Enter m, a, or p: m to move, a to attack, and p to peek into the house\n")
+        if command in commands:
+            if command is commands[0]:
+                print("pick move dir: ")
+                x = int(input("x: "))
+                y = int(input("y: "))
+                game.move((x, y))
+
+            if command is commands[1]:
+                print("choose weapon: \n")
+                game.getPlayer().printWeapons()
+
+                game.attackHouse(game.getPlayerLoc(), int(input("choose: ")))
+
+            if command is commands[2]:
+                game.getNeighborhood().peekHouse(game.getPlayerLoc())
+
+        else:
+            print("invalid command")
