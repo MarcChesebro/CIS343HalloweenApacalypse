352540796109d4d97b2f6838acd9a3aa6fb9c2c7
Marc Chesebro <marc.chesebro@gmail.com>
2017-11-13 17:21:31 -0500

    added more feedback on invalid moves/commands

--- HalloweenApocalypse.py
+++ HalloweenApocalypse.py
@@ -8,6 +8,9 @@ commands = ["m", "a", "p"]
 if __name__ == "__main__":
 
     while True:
+        if not game.getStatus():
+            break
+
         print("location: {}".format(game.getPlayerLoc()))
         command = input("Enter m, a, or p: m to move, a to attack, and p to peek into the house\n")
         if command in commands:
@@ -15,7 +18,8 @@ if __name__ == "__main__":
                 print("pick move dir: ")
                 x = int(input("x: "))
                 y = int(input("y: "))
-                game.move((x, y))
+                if not game.move((x, y)):
+                    print("invalid move")
 
             if command is commands[1]:
                 print("choose weapon: \n")
@@ -28,3 +32,4 @@ if __name__ == "__main__":
 
         else:
             print("invalid command")
+    print("you lost!")
