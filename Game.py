99e4830bb296e1a6f7a0a4bb0493902d1fea48b4
Marc Chesebro <marc.chesebro@gmail.com>
2017-11-13 10:16:54 -0500

    implemented getPlayerLoc and improved move function

--- Game.py
+++ Game.py
@@ -2,14 +2,28 @@ from Entities.Player import Player
 from Neighborhood import Neighborhood
 
 class Game(object):
+    """
+    A simple class that hold the player and the Neighborhood and provides
+    functionality to interact with them
+    """
 
     def __init__(self):
-        self.player = Player()
-        self.playerLoc = (0, 0)
-        self.neighborhood = Neighborhood()
 
-        def move(self, dir):
-            if dir[0] in range(0, 1) && dir[1] in range(0, 1):
-                newLoc =  (self.playerLoc[0] + dir[0], self.playerLoc[1] + dir[1])
-                if self.neighborhood.inBounds(newLoc)
-                    self.playerLoc = newLoc
+        self._player = Player()
+        self._playerLoc = (0, 0)
+        self._neighborhood = Neighborhood()
+
+    def getPlayerLoc(self):
+        return self._playerLoc
+
+    def move(self, dir):
+
+        valid = [-1, 0, 1]
+        if dir[0] in valid and dir[1] in valid:
+            newLoc =  (self._playerLoc[0] + dir[0], self._playerLoc[1] + dir[1])
+            if self._neighborhood.inBounds(newLoc):
+                self._playerLoc = newLoc
+
+    def attackHouse(self, loc, weaponNum):
+        self._player.attackAll(self._neighborhood.getHouse(loc), weaponNum)
+        self._neighborhood.getHouse(loc).dealDamage(self._player)
